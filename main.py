61a859b6f0d7bde32acfa301077c9d226077f05e
Ashique Uk <ashiqueuk@Ashiques-MacBook-Air.local>
2026-06-03 23:09:31 +0530

    Day 2

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ products = [
     product(id=2, name="S26 Ultra", disc="Best Phone", price=130000.00, quant=10),
     product(id=3, name="X300 Pro", disc="Best Phone", price=1000000.00, quant=10),
 ]
-
+#view
 @app.get("/products")
 def get_product():
     return products
 
-#view
+#view by id
 @app.get("/product/{id}")
 def get_product_by_id(id:int):
     for i in products:
@@ -30,4 +30,25 @@ def get_product_by_id(id:int):
 def add_product(product:product):
     products.append(product)
     return product
+
+#update
+@app.put("/product")
+def update_product(id:int, product:product):
+     for i in range(len(products)):
+        if products[i].id==id:
+            products[i]=product
+            return "Product added Success."
+     return "Not Found."
+
+#del
+@app.delete("/product")
+def del_product(id:int):
+    for i in range(len(products)):
+        if products[i].id==id:
+            del products[i]
+            return "Product Deleted"
+    return "Not Found."
+        
+        
+
 
